d3d9d6d5a0bb3224b03bb26a25dd43f7feb5b19e
yaskevich <alyaxey.yaskevich@gmail.com>
2021-04-18 23:57:45 +0300

    render Login view [front]

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,5 +1,20 @@
 <template>
-  login
+  <div class="p-container " style="text-align:center;max-width:10rem;">
+  <div class="p-field p-float-label">
+      <InputText id="user-id" type="username" aria-describedby="user-id-help" class="p-invalid"  v-model="user.email" />
+      <label for="user-id">Электронная почта</label>
+      <small id="user-id-help" class="p-error">Username is not available.</small>
+  </div>
+
+  <div class="p-float-label p-mt-6">
+  	<Password v-model="user.password" :feedback="false" toggleMask id="pwd"/>
+  	<label for="pwd">Пароль</label>
+  </div>
+
+  <div class="p-mt-6">
+    <Button label="Войти" @click="confirm" />
+  </div>
+
   <!-- <el-form label-width="120px" ref="formRef" :model="user" :inline="true" :rules="rules">
     <el-form-item prop="email">
       <el-input placeholder="E-mail" v-model="user.email"></el-input>
@@ -10,15 +25,17 @@
     <el-button type="primary" @click="confirm">Войти</el-button>
     <div class="error">{{error}}</div>
   </el-form> -->
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, reactive, ComponentPublicInstance } from 'vue';
+import Password from 'primevue/password';
 import store from "../store";
 
 export default defineComponent({
   setup() {
-    const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
+    // const formRef = ref<ComponentPublicInstance<typeof ElForm>>();
     let user = reactive({email: '', password: ''});
     const error = ref('');
 
@@ -26,6 +43,7 @@ export default defineComponent({
     //   formRef.value?.resetFields();
     // };
     const confirm = async () => {
+        console.log("user/pass");
       // formRef.value?.validate(async(valid) => {
       //   if (valid) {
       //     console.log("ok send", user);
@@ -43,13 +61,27 @@ export default defineComponent({
     };
 
 
-    return { formRef, confirm, user, error };
+    return { confirm, user, error };
   },
+  components: {
+    Password
+  }
 });
 </script>
 
-<style scoped>
-.error {
-  color: red;
+<style lang="scss" scoped>
+.sizes {
+    .p-inputtext {
+        display: block;
+        margin-bottom: .5rem;
+
+        &:last-child {
+            margin-bottom: 0;
+        }
+    }
 }
+
+// .p-field * {
+//     display: block;
+// }
 </style>

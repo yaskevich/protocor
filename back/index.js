8542ad1640466bc2a364c30e775c60b789778b53
yaskevich <alyaxey.yaskevich@gmail.com>
2021-06-01 00:05:22 +0300

    implement caching [back]

--- back/index.js
+++ back/index.js
@@ -10,16 +10,21 @@ import passport from 'passport';
 import passportJWT from "passport-jwt";
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
+import persistentCache from 'persistent-cache';
 // import db from './db.js';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 (async () => {
+	const cache = persistentCache();
 	const app = express();
 	const port = process.env.PORT || 3061;
 	const JWTStrategy   = passportJWT.Strategy;
 	const ExtractJWT = passportJWT.ExtractJwt;
+	console.log("cache keys:", cache.keysSync());
+
+	const serializeQuery = (query) => Object.entries(query).map( x => x.join('') ).join('');
 
 	const createToken = (user) => {
 	  return jwt.sign({
@@ -122,93 +127,118 @@ const __dirname = path.dirname(__filename);
 	});
 
 	app.post('/api/freq', async(req, res) => {
-		console.log("POST", req.body);
-		if (req.body.token) {
-			const params = {
-				"mode": req.body.corpus || "main",
-        "lang": "ru",
-        "sort": "i_grtagging",
-        "sr": "1",
-        "lex1": req.body.token,
-        "g": "i_doc",
-        "startyear": "1800",
-        "dpp": "10",
-        "spp": "10",
-        "text": "lexgramm",
-        "smoothing": "3",
-        "level1": "0",
-        "graphic_from_result": "1",
-        "endyear": "2019",
-        "parent1": "0",
-        "nodia": "1",
-        "sem-mod1": "sem",
-        "spd": "10",
-        "out": "normal",
-        "format": "json",
-      };
+		console.log(req.method, req.url, req.body);
+		const cacheKey = serializeQuery(req.body);
+		const datum = cache.getSync(cacheKey);
 
-			try {
-	    	const response = await axios.get('https://processing.ruscorpora.ru/graphic.xml', { params: params });
-	    	// console.log(response.data.values[0].data);
-				res.json({ "freq": response.data.values[0].data});
-		  } catch (error) {
-		    console.error(error);
-				res.json({ "error": error });
-		  }
+		if (datum && Object.keys(datum)) {
+			 console.log("cached", req.body.token);
+			 res.json(datum);
 		} else {
-				res.json({ "error": "empty query" });
+			if (req.body.token) {
+				const params = {
+					"mode": req.body.corpus || "main",
+	        "lang": "ru",
+	        "sort": "i_grtagging",
+	        "sr": "1",
+	        "lex1": req.body.token,
+	        "g": "i_doc",
+	        "startyear": "1800",
+	        "dpp": "10",
+	        "spp": "10",
+	        "text": "lexgramm",
+	        "smoothing": "3",
+	        "level1": "0",
+	        "graphic_from_result": "1",
+	        "endyear": "2019",
+	        "parent1": "0",
+	        "nodia": "1",
+	        "sem-mod1": "sem",
+	        "spd": "10",
+	        "out": "normal",
+	        "format": "json",
+	      };
+
+				try {
+		    	const response = await axios.get('https://processing.ruscorpora.ru/graphic.xml', { params: params });
+		    	// console.log(response.data.values[0].data);
+					const returnResults = response.data.values[0].data;
+					cache.putSync(cacheKey, returnResults);
+					res.json({ "freq": returnResults });
+			  } catch (error) {
+			    console.error(error);
+					res.json({ "error": error });
+			  }
+			} else {
+					res.json({ "error": "empty query" });
+			}
 		}
 	});
 
 	app.post('/api/text', async(req, res) => {
-		console.log("POST", req.body);
+		console.log(req.method, req.url, req.body);
 		if (req.body.id) {
-			const params = {
-				"mode": req.body.corpus || "main",
-				"text": "document-info",
-				"docid": req.body.id,
-				"format": "json",
-			};
+			const datum = cache.getSync(req.body.id);
+			if (datum && Object.keys(datum)) {
+				 console.log("cached", req.body.id);
+				 res.json(datum);
+			 } else {
+					const params = {
+						"mode": req.body.corpus || "main",
+						"text": "document-info",
+						"docid": req.body.id,
+						"format": "json",
+					};
 
-			try {
-	    	const response = await axios.get('https://processing.ruscorpora.ru/search-explan.xml', { params: params });
-	    	// console.log(response);
-				res.json(response.data);
-		  } catch (error) {
-		    console.error(error);
-				res.json({ "error": error });
-		  }
+					try {
+			    	const response = await axios.get('https://processing.ruscorpora.ru/search-explan.xml', { params: params });
+			    	// console.log(response);
+						cache.putSync(req.body.id, response.data);
+						res.json(response.data);
+				  } catch (error) {
+				    console.error(error);
+						res.json({ "error": error });
+				  }
+			}
 		} else {
 				res.json({ "error": "empty query" });
 		}
 	});
 
 	app.post('/api/query', async(req, res) => {
-		console.log("POST [query]", req.body);
-		if (req.body.token) {
-				let pageCurrent = 0;
-				let pageLast  = 1;
-				const returnResults  = { "corp_stat": {}, "found_stat": {}, "documents": [], };
-				while(pageCurrent !== pageLast) {
-						const results = await getResults(req.body.token, req.body.corpus, req.body.dpp, req.body.spd, pageCurrent);
-						if(results) {
-							pageCurrent = results["pager_info"]["current_page_num"];
-							pageLast  = results["pager_info"]["last_page_num"];
-							returnResults["corp_stat"] = results["corp_stat"];
-							returnResults["found_stat"] = results["found_stat"];
-							// returnResults.documents.push(...results.documents);
-							// external API was changed !!!
-							returnResults.documents.push(...results.document_groups.flat());
-							console.log(`${pageCurrent} of ${pageLast} pages`);
-							if(!req.body.full) { break; }
-						}
-				}
-				res.json(returnResults);
+		console.log(req.method, req.url, req.body);
+		const cacheKey = serializeQuery(req.body);
+		const datum = cache.getSync(cacheKey);
+
+		if (datum && Object.keys(datum)) {
+			 console.log("cached", req.body.token);
+			 res.json(datum);
 		} else {
-				res.json({ "error": "empty query" });
+			if (req.body.token) {
+					let pageCurrent = 0;
+					let pageLast  = 1;
+					const returnResults  = { "corp_stat": {}, "found_stat": {}, "documents": [], };
+					while(pageCurrent !== pageLast) {
+							const results = await getResults(req.body.token, req.body.corpus, req.body.dpp, req.body.spd, pageCurrent);
+							if(results) {
+								pageCurrent = results["pager_info"]["current_page_num"];
+								pageLast  = results["pager_info"]["last_page_num"];
+								returnResults["corp_stat"] = results["corp_stat"];
+								returnResults["found_stat"] = results["found_stat"];
+								// returnResults.documents.push(...results.documents);
+								// external API was changed !!!
+								returnResults.documents.push(...results.document_groups.flat());
+								console.log(`${pageCurrent} of ${pageLast} pages`);
+								if(!req.body.full) { break; }
+							}
+					}
+					cache.putSync(cacheKey, returnResults);
+					res.json(returnResults);
+			} else {
+					res.json({ "error": "empty query" });
+			}
 		}
 	});
-
 
 	app.listen(port);
 	// console.log(`Running at port ${port}`);

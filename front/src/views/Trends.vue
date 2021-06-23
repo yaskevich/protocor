84b71f98d3826e7531530564b0b179d8b58e71e4
yaskevich <alyaxey.yaskevich@gmail.com>
2021-06-23 19:18:09 +0300

    remove direct API query from Trends [front]

--- front/src/views/Trends.vue
+++ front/src/views/Trends.vue
@@ -15,32 +15,28 @@
 
 <script>
 
+  import { ref, reactive, onBeforeMount, defineComponent, watch } from 'vue';
   import Chart from '../components/Chart.vue';
   import Chip from 'primevue/chip';
-  import { ref, reactive, onBeforeMount, defineComponent, watch } from 'vue';
-  import axios from 'axios';
+  import store from '../store';
 
   export default defineComponent({
     setup() {
       const token = ref('');
       const freq = reactive({ data: [], start: 1800, end: 2021 });
       const tokens = reactive([]);
-      const tokensFreqs = reactive({});
 
-      onBeforeMount(async() => {
+      onBeforeMount(async () => {
         tokens.push('котик', 'лиса');
-
       });
 
       watch(
         () => tokens.length,
-        (count, prevCount) => {
+        async (count, prevCount) => {
           console.log('tokens len', count);
           for (let i = 0; i < tokens.length; i++) {
-            if(!(tokens[i] in tokensFreqs)) {
-              console.log("get", tokens[i]);
-              tokensFreqs[tokens[i]] = "ok";
-            }
+            // console.log('get', tokens[i]);
+            await store.getFreq(tokens[i]);
           }
         }
       );
@@ -61,27 +57,7 @@
         tokens.splice(tokens.indexOf(val), 1);
       };
 
-      const renderChart = async e => {
-        if (tokens.value.length) {
-          try {
-            const config = {
-              // headers: { Authorization: "Bearer " + state.token },
-            };
-            console.log('tokens', tokens.value);
-            const response = await axios.post('/api/freq', { token: tokens.value[0] }); // config);
-            // resp.value = response.data;
-            console.log('chart', response.data);
-            freq.start = Number(response.data['freq'][0][0]);
-            freq.end = Number(response.data['freq'].slice(-1)[0][0]);
-            // freq.data = response.data['freq'].map(Array.prototype.shift);
-            freq.data = response.data['freq'].map(x => x[1]);
-            console.log('freq', freq);
-          } catch (error) {
-            console.log('Cannot get data via API', error);
-            return error;
-          }
-        }
-      };
+      const renderChart = async e => {};
 
       return {
         removeToken,

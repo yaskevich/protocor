ea3dbebb1f51d9e19ee2da598980187296626e34
yaskevich <alyaxey.yaskevich@gmail.com>
2021-07-22 23:56:36 +0300

    create Profile view [front]

---
+++ front/src/views/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div>
+    ....
+  </div>
+
+</template>
+
+<script>
+
+  import { ref, reactive, defineComponent, } from 'vue';
+  import store from '../store';
+
+  export default defineComponent({
+    setup() {
+
+      return {
+      };
+    },
+    components: {
+    },
+  });
+
+</script>

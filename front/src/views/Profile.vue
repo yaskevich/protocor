c44896bb6487cc65da1cb3fb6248de638e63241c
yaskevich <alyaxey.yaskevich@gmail.com>
2021-07-30 00:06:17 +0300

    load userlogs in Profile [front]

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -1,20 +1,34 @@
 <template>
 
-  <div>
-    ....
+  <div class="p-p-2">
+      {{profile.firstname}} {{profile.lastname}}
+  </div>
+  <div class="p-p-2">
+      {{profile.email}}
   </div>
 
 </template>
 
 <script>
 
-  import { ref, reactive, defineComponent, } from 'vue';
+  import { ref, reactive, defineComponent, onBeforeMount, } from 'vue';
   import store from '../store';
 
   export default defineComponent({
     setup() {
+      const history = ref([]);
+
+      onBeforeMount(async () => {
+
+        if (store.state.profile?.id) {
+          history.value = await store.getData('userlogs/');
+          console.log("hist", history.value);
+        }
+
+      });
 
       return {
+        profile: store.state.profile,
       };
     },
     components: {

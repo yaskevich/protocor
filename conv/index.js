a6aa218171a88e458ed47a680be397e9c406d89b
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-23 17:19:08 +0300

    create tables [conv]

--- conv/index.js
+++ conv/index.js
@@ -4,10 +4,41 @@ import fs from 'fs';
 import path from 'path';
 import csv from 'async-csv';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import pg from 'pg';
+const { Pool } = pg;
+const pool = new Pool();
+
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const idsArray = JSON.parse(fs.readFileSync('identifiers.json', 'utf8'));
+const ids = Object.fromEntries(idsArray.map(x => [x.id, x]));
+
+const featuresId = [];
+
+const columns = ['path', 'header', 'created', 'editor_id'];
+
+const schemes = [
+	`DROP TABLE IF EXISTS texts`,
+	`CREATE TABLE IF NOT EXISTS texts (id SERIAL PRIMARY KEY, filepath text, header text, created text, features json, editor_id integer)`,
+	`DROP TABLE IF EXISTS features`,
+	`CREATE TABLE IF NOT EXISTS features (
+			id SERIAL PRIMARY KEY,
+			groupid text,
+			ru text not null,
+			en text,
+			UNIQUE (groupid, ru)
+	)`,
+];
+
+
+const getRuTitle = (x) => { return ids?.[x]?.['ru'] || x; }
+	
 
 async function processFile(fileName) {
 
@@ -23,7 +54,17 @@ async function processFile(fileName) {
         }
 
         const fieldRow = csvArr.shift();
+		// console.log(fieldRow);
+
 		let rowNumber = 0;
+
+		for (let table in schemes){
+			// console.log(table);
+			// await pool.query('DROP table IF EXISTS ' + table + ' CASCADE');
+			await pool.query(schemes[table]);
+		}
+		await pool.end();
+
 
         // for (const row of csvArr) {
 		for (let n = 0; n < csvArr.length; n++) {
@@ -35,8 +76,17 @@ async function processFile(fileName) {
             
             for (let i=0; i < row.length; i++) {
                 const data  = row[i];
-                
+                // getRuTitle
+				if(data) {
+					console.log(`${fieldRow[i]} [${getRuTitle(fieldRow[i])}] ${data}`);
+				}
             }
+			
+			
+			if(rowNumber > 1) {
+				return;
+			}
+				
     
         }
         
@@ -47,7 +97,7 @@ async function processFile(fileName) {
     } else {
         console.log("Path to the file with data is incorrect!");
     }
-
+	
 }
 // entry point
 (async () => { 

39496952c82bfe65db420d23145f443eeddf1637
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-27 23:06:39 +0300

    remove dummy stuff from About view [front]

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="about">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <p>Экспериментальные эксперименты с интерфейсом<br/> <a href="https://ruscorpora.ru" target="_blank">Национального корпуса русского языка</a>.</p>
+    <p>
+      Не пытайтесь повторить это дома!
+    </p>
+    <p>
+      Проводит <a href="https://yaskevich.com/" target="_blank">Алексей Яскевич</a> (Лаб. № 20 ИППИ РАН)
+    </p>
   </div>
 </template>
 

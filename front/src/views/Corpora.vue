9c07992ce036503520fbddef48219eb9e66298af
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-27 23:03:54 +0300

    add Corpora view [front]

---
+++ front/src/views/Corpora.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    test
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Search from '@/components/Search.vue'; // @ is an alias to /src
+
+@Options({
+  components: {
+    Search,
+  },
+})
+export default class Home extends Vue {}
+</script>

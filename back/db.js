2935fe9a7ba3de5f2b1a2fe60d0c05ca9760da45
yaskevich <alyaxey.yaskevich@gmail.com>
2021-08-29 16:26:28 +0300

    create function getFeaturesDict [back]

--- back/db.js
+++ back/db.js
@@ -123,11 +123,15 @@ export default {
 		return res.rows;
 	},
 	async getFeaturesUnique() {
-    // const query  = "select array_agg(distinct u.val) uniquefeatures from texts t cross join lateral unnest(t.features) as u(val)";
     const query  = "SELECT prop, count(prop) FROM texts, unnest(features) AS prop group by prop";
 		const res = await pool.query(query);
-		// return res?.rows[0]?.uniquefeatures;
 		return res?.rows;
 	},
-
+	async getFeaturesDict() {
+    // const query  = "select array_agg(distinct u.val) uniquefeatures from texts t cross join lateral unnest(t.features) as u(val)";
+    const query  = "select id, groupid, ru from features where id in (select unnest(array_agg(distinct u.val)) uniquefeatures from texts t cross join lateral unnest(t.features) as u(val))";
+		const res = await pool.query(query);
+    // return res?.rows[0]?.uniquefeatures;
+		return res?.rows;
+	},
 };

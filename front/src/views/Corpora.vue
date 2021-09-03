cb26de053871ecefb8ee4c2543a3303413707b02
yaskevich <alyaxey.yaskevich@gmail.com>
2021-09-04 00:13:36 +0300

    render percentage of texts in stats in Corpora [front]

--- front/src/views/Corpora.vue
+++ front/src/views/Corpora.vue
@@ -1,12 +1,12 @@
 <template>
 
-  <div class="p-pb-4">
-    Корпус содержит <span class="p-text-bold">{{count}}</span> текст{{countMascSg(count)}}
+  <div class="p-pb-4" v-if="count">
+    Корпус содержит <span class="p-text-bold">{{zeropad(count)}}</span> текст{{countMascSg(count)}}
   </div>
   <div v-for="v in dict">
     <div class="p-grid p-jc-center p-mb-4">
       <Panel :header="v[0]['groupid']" style="min-width:300px;">
-        <div v-for="obj in v" class="p-grid p-ai-center vertical-container p-pb-2">
+        <div v-for="obj in v" class="p-grid p-ai-center vertical-container p-pb-2" :title="percent(counts.get(obj.id), count)">
           <div class="p-col p-text-left">{{obj.ru}}</div>
           <div class="p-col p-text-right">{{counts.get(obj.id)}}</div>
         </div>
@@ -40,6 +40,10 @@
         return last == 1 ? '' : ['2', '3', '4'].includes(last) ? 'а' : 'ов';
       };
 
+      const percent = (num, all) => {
+           return ((num/all) * 100).toFixed(2);
+      };
+
       onBeforeMount(async () => {
         const data = await store.getData('features/spoken');
         // console.log("data", data.value);
@@ -53,6 +57,8 @@
         counts,
         count,
         countMascSg,
+        zeropad: store.space000,
+        percent,
       };
     },
     components: {},

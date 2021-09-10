9fd825e46c75e135d2ab2a1fab1ef2baf7919f98
yaskevich <alyaxey.yaskevich@gmail.com>
2021-09-10 23:31:47 +0300

    store metafields in database [conv]

--- conv/meta.js
+++ conv/meta.js
@@ -1,4 +1,4 @@
-'use strict';
+	'use strict';
 
 import fs from 'fs';
 import path from 'path';
@@ -6,6 +6,13 @@ import csv from 'async-csv';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import pg from 'pg';
+const { Pool } = pg;
+const pool = new Pool();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -32,12 +39,22 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 		  process.exit();
 		}
 
-		
+		const tableScheme = `CREATE TABLE IF NOT EXISTS metafields (
+				id SERIAL PRIMARY KEY,
+				name text,
+				ru text not null,
+				en text
+		)`;
+
+		await pool.query('DROP table IF EXISTS metafields');
+		await pool.query(tableScheme);
+
+
 		csvArr.shift();
 		const fieldRow = csvArr.shift();
 		console.log(fieldRow);
-		
 
+		const colsToRu = {};
 
 		let rowNumber = 0;
 
@@ -46,8 +63,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 		  // rowNumber = `${n + 2}(${n})`;
 		  rowNumber = n + 2;
 		  const row = csvArr[n];
-		  
-		  console.log(row[0].replace(/\n|\r\n/g, ''), row[1].replace(/\n|\r\n/g, ''));
+
+			const [key, val] = row.map (x => x.replace(/\n|\r\n/g, ''));
+			colsToRu[key] = val;
+
+			await pool.query(`INSERT INTO metafields (name, ru) VALUES($1, $2) RETURNING id`, [key, val]);
+
 		  // const values = [];
 		  // let skip = false;
 
@@ -68,6 +89,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 		}
 
+		await pool.end();
 		console.log("...done");
 	} else {
 		console.log("Path to the file with data is incorrect!");

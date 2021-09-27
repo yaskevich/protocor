b62ecd9a85a9ae9b4fdacc576e2bdb060e808649
yaskevich <alyaxey.yaskevich@gmail.com>
2021-09-27 22:38:19 +0300

    reroute non-logged-in user to Login in Profile [front]

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -13,6 +13,7 @@
 
   import { ref, reactive, defineComponent, onBeforeMount, } from 'vue';
   import store from '../store';
+  import router from '../router';
 
   export default defineComponent({
     setup() {
@@ -23,6 +24,8 @@
         if (store.state.profile?.id) {
           history.value = await store.getData('userlogs/');
           console.log("hist", history.value);
+        } else {
+          router.push('/login');
         }
 
       });
